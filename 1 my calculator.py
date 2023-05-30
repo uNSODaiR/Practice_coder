616c6e5e3a2edd566d216815871cec03e7e1c4bc
unsodair.wallet <99089593+uNSODaiR@users.noreply.github.com>
2023-05-30 22:02:06 +0530

    Update 1 my calculator.py

--- 1 my calculator.py
+++ 1 my calculator.py
@@ -1,59 +1,17 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "10.0\n"
-     ]
-    }
-   ],
-   "source": [
-    "num1 = input(\"Enter the 1st number: \")\n",
-    "num2 = input(\"Enter the 2nd number: \")\n",
-    "calcu = input(\"+, -, * or /\")\n",
-    "\n",
-    "if calcu == \"+\":\n",
-    "    result = (float(num1) + float(num2))\n",
-    "elif calcu == \"-\":\n",
-    "    result =  (float(num1) - float(num2))\n",
-    "elif calcu == \"*\":\n",
-    "    result =  (float(num1) * float(num2))\n",
-    "elif calcu == \"/\":\n",
-    "    result =  (float(num1) / float(num2))\n",
-    "else:\n",
-    "    print (\"INVALID INPUT - Please enter proper number\")\n",
-    "    result = None # none means no value whereas false means its wrong\n",
-    "if result != None: # used != instead of is not\n",
-    "    print (result)\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.3"
-  },
-  "orig_nbformat": 4
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+num1 = input("Enter the 1st number: ")
+calcu = input("+, -, * or /")
+num2 = input("Enter the 2nd number: ")
+
+if calcu == "+":
+    result = (float(num1) + float(num2))
+elif calcu == "-":
+    result =  (float(num1) - float(num2))
+elif calcu == "*":
+    result =  (float(num1) * float(num2))
+elif calcu == "/":
+    result =  (float(num1) / float(num2))
+else:
+    print ("INVALID INPUT - Please enter proper number")
+    result = None # none means no value whereas false means its wrong
+if result != None: # used != instead of is not
+    print (result)
